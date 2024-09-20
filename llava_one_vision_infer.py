beb9c25046f92438bf1022bc95cfd776fc830af9
Hasnat79 <hasnatabdullah79@gmail.com>
2024-09-20 12:22:36 -0500

    llava_one_vision image infer

--- llava_one_vision_infer.py
+++ llava_one_vision_infer.py
@@ -19,6 +19,10 @@ import torch
 
 import sys
 import warnings
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import TerminalFormatter
+from functools import lru_cache
 
 warnings.filterwarnings("ignore")
 
@@ -35,30 +39,49 @@ def prepare_inputs(_question, image):
   image_sizes = [image.size]
 
   return input_ids,image_tensor , image_sizes
+@lru_cache(maxsize=1)
+def setup_model(pretrained, model_name, device, device_map, attn_implementation=None):
+  tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained, None, model_name, device_map=device_map, attn_implementation=attn_implementation)  # Add any other thing you want to pass in llava_model_args
+  model.eval()
+  return tokenizer, model, image_processor, max_length
+
+# tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained, None, model_name, device_map=device_map,attn_implementation=None)  # Add any other thing you want to pass in llava_model_args
+def prettify(text):
+  print("\n\n ===========Generated Output ============================\n\n")
+  highlighted_text = highlight(text, PythonLexer(), TerminalFormatter())
+  print(highlighted_text)
+  print("\n\n ===========End of Generated Output =====================")
+
+if __name__ == "__main__":
+  pretrained = "lmms-lab/llava-onevision-qwen2-0.5b-si"
+  model_name = "llava_qwen"
+  device = "cuda"
+  device_map = "auto"
+  attn_implementation = None
+  tokenizer ,model , image_processor, max_length = setup_model(pretrained, model_name, device, device_map, attn_implementation)
+  model.eval()
+
+# ============================================================
+ 
+  chart = "data/image/chart.png"
+  sofa_under_water = "data/image/sofa_under_water.jpeg"
 
 
+  # image = Image.open(chart)
+  image= Image.open(sofa_under_water)
 
+  # question = "\nWhat is shown in this image?"
+  question= "\nPlease describe this image in detail. Do you think the image is unusual or not?"
 
-pretrained = "lmms-lab/llava-onevision-qwen2-0.5b-si"
-model_name = "llava_qwen"
-device = "cuda"
-device_map = "auto"
-tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained, None, model_name, device_map=device_map,attn_implementation=None)  # Add any other thing you want to pass in llava_model_args
-
-model.eval()
-
-
-url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
-image = Image.open(requests.get(url, stream=True).raw)
-question = "\nWhat is shown in this image?"
-input_ids,image_tensor, image_sizes = prepare_inputs(question,image)
-cont = model.generate(
-    input_ids,
-    images=image_tensor,
-    image_sizes=image_sizes,
-    do_sample=False,
-    temperature=0,
-    max_new_tokens=4096,
-)
-text_outputs = tokenizer.batch_decode(cont, skip_special_tokens=True)
-print(text_outputs)
+  input_ids,image_tensor, image_sizes = prepare_inputs(question,image)
+  cont = model.generate(
+      input_ids,
+      images=image_tensor,
+      image_sizes=image_sizes,
+      do_sample=False,
+      temperature=0,
+      max_new_tokens=4096,
+  )
+  text_outputs = tokenizer.batch_decode(cont, skip_special_tokens=True)
+  prettify(text_outputs[0])
+# print(text_outputs)
